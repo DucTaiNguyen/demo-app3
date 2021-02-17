89c01c6646ea9c7bf182511ace93ef86b35f2fb4
Duc Tai Nguyen <brucelee50002000pro@gmail.com>
2021-02-18 00:23:57 +0700

    third

--- CropMainApp.py
+++ CropMainApp.py
@@ -5,19 +5,121 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
 
 #########--------------- Integrate Flask and Dash-------------------############
 
 import flask 
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo 
 from pymongo import MongoClient
+from flask import Flask, render_template
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+'''
+I use 2 layout="" to see what happen when I add Dash to flask. but they are not really compatible.
+
+The long layout can insert Dash, but not the flask.nav
+
+'''
 
 
+# layout = """
+# <!DOCTYPE html>
+# <html lang="en">
+# <title>{%title%}</title>
+# {%favicon%}
+# {%metas%}
+# <meta charset="UTF-8">
+# <meta name="viewport" content="width=device-width, initial-scale=1">
+# <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+# <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+# <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+# <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+# {%css%}
+# <style>
+# body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+# .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+# .fa-anchor,.fa-coffee {font-size:200px}
+# </style>
+# <body>
+# <!-- Navbar -->
+# <div class="w3-top">
+#   <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+#     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+#     <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+#     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 1</a>
+#     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 2</a>
+#     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 3</a>
+#     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Link 4</a>
+#   </div>
+
+#   <!-- Navbar on small screens -->
+#   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+#     <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 1</a>
+#     <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 2</a>
+#     <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 3</a>
+#     <a href="#" class="w3-bar-item w3-button w3-padding-large">Link 4</a>
+#   </div>
+# </div>
+
+# <!-- Header -->
+# <header class="w3-container w3-red w3-center" style="padding:128px 16px">
+#   <h1 class="w3-margin w3-jumbo">START PAGE</h1>
+#   <p class="w3-xlarge">Template by w3.css</p>
+#   <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Get Started</button>
+# </header>
+
+# <!-- First Grid -->
+# <div class="w3-row-padding w3-padding-64 w3-container">
+#   <div class="w3-content">
+#     <div class="w3-twothird">
+#       <h1>Bar Chart</h1>
+
+#         <h2>Insert bar chart here</h2>
+#         {%app_entry%}
+#   </div>
+# </div>
+#   {%config%}
+#   {%scripts%}
+#   {%renderer%}
+# </body>
+# </html>
+# """
+
+
+
+layout='''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    {% nav.my_navbar.render() THis the problem the nav appear here????  %}
+    <div class="w3-row-padding w3-padding-64 w3-container">
+        <div class="w3-content">
+          <div class="w3-twothird">
+            <h1>Bar Chart</h1>
+      
+              <h2>Insert bar chart here</h2>
+                {%app_entry%}
+        </div>
+  {%config%}
+  {%scripts%}
+  {%renderer%}
+</body>
+</html>
+'''
 # #mongo = PyMongo(server)
 ## flask server is main . There are 2 branches use main server ,these are app and app2
-server = flask.Flask(__name__)
-app = dash.Dash(__name__, server = server, url_base_pathname='/dashboard/')
-app2 = dash.Dash(__name__, server = server, url_base_pathname='/dashboard2/')
+server = flask.Flask(__name__,)
+app = dash.Dash(__name__, server = server,external_stylesheets=[] ,url_base_pathname='/dashboard/',)
+app2 = dash.Dash(__name__, server = server,external_stylesheets=external_stylesheets ,index_string=layout) # index_string=layout url_base_pathname='/dashboard2/'
+app3 = dash.Dash(__name__, server = server,external_stylesheets=[] ,url_base_pathname='/dashboard3/')
 
 
 
@@ -31,19 +133,23 @@ nav.register_element('my_navbar',Navbar(
     'thenav',
     View('CropParameters','render_dashboard'),
     View('CropParameters_All','render_dashboard2'),
+    View('CropParameters 3','render_dashboard3'),
     View('Item One', 'item',item=1),
     Link('Google','https://www.google.com'),
 
     ))
 
 
-@server.route('/')
+
+
+
+@server.route('/index')
 def index():
     return render_template('index.html')
 
-@server.route('/items/<item>')
-def item(item):
-    return '<h1> The item Page!!! the item is: {}.'.format(item)
+# @server.route('/item')
+# def item():
+#     return render_template('layout.html')
 
 #######------------------ Make some routes demo------------------------######
 
@@ -55,6 +161,10 @@ def render_dashboard():
 @server.route('/plotly_dashboard')
 def render_dashboard2():
     return flask.redirect('/dashboard2/')
+
+@server.route('/plotly_dashboard3')
+def render_dashboard3():
+    return flask.redirect('/dashboard3/')
 
 
 #Loading data
@@ -90,8 +200,8 @@ def data(product: str):
 
 
 
-# # ----------------------Fig2 and App2 layout------------------------#
 
+#####------ For testing ----------#########
 fig2 = px.bar(df, x=df.xtime, y=df.columns[4:9])
 fig2.update_layout(
     
@@ -99,18 +209,60 @@ fig2.update_layout(
     yaxis_title="value",
     xaxis_title="Time",
     legend_title="variable",
+    
    )
+# fig3 = px.bar(df, x=df.xtime, y=df.columns[4:9])
+# fig3.update_layout(
+    
+#     title="CropParameters Div 2",
+#     yaxis_title="value",
+#     xaxis_title="Time",
+#     legend_title="variable",
+#    )
+#####################################################
+##########--------------app3------------#############
+'''
+stem_elong,stem_thick,cum_trusses,stem_dens,plant_dens
+'''
+app3.layout = html.Div(children=[
+    html.Div([
+        html.H1(["Crop Parameters of stem_elong".upper()], style={'text-align': 'center'}),
+        dcc.Graph(id='my_app3_Graph_1', figure=go.Figure(data("stem_elong")))
+    ]),
+
+        html.Div([
+        html.H1(["Crop Parameters of stem_thick".upper()], style={'text-align': 'center'}),
+        dcc.Graph(id='my_app3_Graph_2', figure=go.Figure(data("stem_thick")))
+    ]),
+        html.Div([
+        html.H1(["Crop Parameters of cum_trusses".upper()], style={'text-align': 'center'}),
+        dcc.Graph(id='my_app3_Graph_3', figure=go.Figure(data("cum_trusses")))
+    ]),
+        html.Div([
+        html.H1(["Crop Parameters of stem_dens".upper()], style={'text-align': 'center'}),
+        dcc.Graph(id='my_app3_Graph_4', figure=go.Figure(data("stem_dens")))
+    ]),
+        html.Div([
+        html.H1(["Crop Parameters of plant_dens".upper()], style={'text-align': 'center'}),
+        dcc.Graph(id='my_app3_Graph_5', figure=go.Figure(data("plant_dens")))
+    ]),
 
 
-app2.layout = html.Div([html.H1('CroParameters for All'.upper()),
-
-
-                    dcc.Graph(id='my-Graph-2', figure=fig2)
 ])
 
+#####----------------- App2----------############
+app2.layout = html.Div([html.H1('CroParameters for All'.upper()),
+                     html.Div(dcc.Input(id='input-on-submit', type='text')),
+                    html.A('Google',href='#'),
+                    dcc.Location(id='url', refresh=False),
+                    html.Br(),
+                      dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
 
+                    dcc.Graph(id='my-Graph-2', figure=fig2),
+                    
+])
 
-# #
 # # ----------------------App layout------------------------#
 
 
@@ -149,4 +301,5 @@ if __name__ == '__main__':
     server.run()        ### This is Flask server
     app.run_server()
     app2.run_server()
+    app3.run_server()
 
